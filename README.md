58dece21bd6a07ffdb44d7839c09bde271d966d7
Tristan Perry <tristan@tristanperry.com>
2019-11-02 12:37:52 +0000

    Added params support

--- README.md
+++ README.md
@@ -1,27 +1,33 @@
-# Domain Name Generator
+# Domain Name Generator (and availability checker)
 
 A simple Python command line domain name generator which I wrote after finding quite a lot of the web-based tools lacking for my purposes, which was:
 
  * generate a simple two word domain.
  * only look at .com.
- * don't report already registered (or 'premium') domains.
+ * don't report registered (or 'premium') domains.
+
+This tool generates keywords based on the provided arguments and then checks if they are registered or not (and currently only outputs available ones).
  
 Usage is command line:
 
 ```
-$ python generatedomain.py kw1 kw2 kw3
-kw1kw2.com is available
-kw1kw3.com is available
-kw2kw1.com is available
-kw2kw3.com is available
-kw3kw1.com is available
-kw3kw2.com is available
+$ python generatedomain.py
+usage: generatedomain.py [-h] [--allow-duplicates]
+                         [--number-words NUMBER_WORDS] --kws KWS [KWS ...]
+generatedomain.py: error: the following arguments are required: --kws
+
+$ python generatedomain.py --allow-duplicates --number-words 2 --kws some awesome keywords
+somekeywords.com is available
+awesomekeywords.com is available
+keywordssome.com is available
+keywordsawesome.com is available
+
 ```
 
 This tool creates all possible non-repeating combinations from the provided keywords. As per the TODOs below, I will make this tool a bit more flexible shortly.
 
 ## TODOs
 
- * Choose how many words can be chosen (i.e. so that 3+ word domains can be generated).
- * Allow for repeating words to be tested for availability (i.e. so that "kw1kw1.com" could be a candidate).
- * Allow for keyword variations (maybe by entering something like `python generatedomain.py tech,techno,techy kw2 kw3).
+ * Allow for keyword variations (probably by allowing something like `python generatedomain.py tech,techno,techy kw2 kw3` - better than trying to be smart by adding "no", "ie", "y" onto the end of keywords and usually getting it wrong).
+ * An option to skip the WHOIS check.
+ * An option to also output taken domains (along with their expiry date).
--- generatedomain.py
+++ generatedomain.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env  python
 
+import argparse
 import itertools
 import re
 import socket
@@ -7,6 +8,10 @@ import sys
 
 
 def whois_request(domain, server, port=43):
+    """
+    Carries out the WHOIS request for a particular domain name, against a particular registrar.
+    This is not .com specific.
+    """
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((server, port))
     sock.send(("%s\r\n" % domain).encode("utf-8"))
@@ -20,6 +25,7 @@ def whois_request(domain, server, port=43):
 
 
 def keyword_free(keyword):
+    """ 'Builds' the domain name (just appends .com as that's all I check right now) then checks its WHOIS status """
     domain_name = keyword + '.com'
     whois_response = whois_request(domain_name, 'whois.verisign-grs.com')
 
@@ -29,9 +35,30 @@ def keyword_free(keyword):
         return True
 
 
-combinations = list(itertools.product(sys.argv[1:], repeat=2))  # build all combinations
-keywords = [value for value in combinations if value[0] != value[1]]  # omit any duplicates
-for kw_tuple in keywords:
-    kw = ''.join(str(i) for i in kw_tuple)
-    if keyword_free(kw):
-        print(kw + '.com is available')
+def parse_cli_args():
+    """ Parses the command line arguments to (e.g.) keep duplicate combinations. """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--allow-duplicates", default=True, help="Allows for keywords to be re-used in the generated domain name",
+        action="store_true"
+    )
+
+    parser.add_argument("--number-words", default=2, type=int, help="numbers of words in the domain candidates")
+
+    parser.add_argument('--kws', nargs='+', help='<Required> Provide one or more keywords', required=True)
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_cli_args()
+
+    combinations = list(itertools.product(args.kws, repeat=args.number_words))  # build all combinations
+    keywords = [value for value in combinations if
+                args.allow_duplicates or (value[0] != value[1] and not args.allow_duplicates)]  # optionally omit dups
+
+    for kw_tuple in keywords:
+        kw = ''.join(str(i) for i in kw_tuple)
+        if keyword_free(kw):
+            print(kw + '.com is available')
